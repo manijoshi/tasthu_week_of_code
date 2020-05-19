647248471fe561557697f73edf60164c85b5bbe3
manijoshi <65389710+manijoshi@users.noreply.github.com>
2020-05-19 22:50:07 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,5 @@
+cost_price=int(input("enter cost of the product "))
+selling_price=int(input("enter selling price of the product "))
+print("profit is",selling_price-cost_price)
+sp=1.05*(selling_price-cost_price)+cost_price
+print("after increment of 5% the selling price of the product would be",sp)
